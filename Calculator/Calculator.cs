78b40836e0c2b40cc8f08f9a421de9c4e633f582
paulmcphailstevenson <paulmstevenson@pm.me>
2023-08-10 21:11:05 +0100

    Added basic fuctions and tests

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -1,13 +1,11 @@
 ﻿namespace Calculator;
-public class Calculator
+public class Calc
 {
-    public int Add(int num1, int num2)
-    {
-        return num1 + num2;
-    }
+    public int Add(int num1, int num2) => num1 + num2;
 
-    public int Substract(int num1, int num2)
-    {
-        return num1 - num2;
-    }
+    public int Subtract(int num1, int num2) => num1 - num2;
+
+    public int Multiply(int num1, int num2) => num1 * num2;
+
+    public int Divide(int num1, int num2) => num1 / num2;
 }
---
+++ CalculatorTests/CalculatorTests.cs
@@ -0,0 +1,61 @@
+using Calculator;
+namespace CalculatorTests;
+
+public class CalculatorTests
+{
+    [Theory]
+    [InlineData(1, 1, 2)]
+    public void Add_ShouldAdd(int num1, int num2, int expected)
+    {
+        // Arrange
+        var calculator = new Calc();
+
+        // Act
+        var result = calculator.Add(num1, num2);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData(1, 1, 0)]
+    public void Substract_ShouldSubtract(int num1, int num2, int expected)
+    {
+        // Arrange
+        var calculator = new Calc();
+
+        // Act
+        var result = calculator.Subtract(num1, num2);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData(1, 1, 1)]
+    public void Multiply_ShouldMultiply(int num1, int num2, int expected)
+    {
+        // Arrange
+        var calculator = new Calc();
+
+        // Act
+        var result = calculator.Multiply(num1, num2);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData(1, 1, 1)]
+    public void Divide_ShouldDivide(int num1, int num2, int expected)
+    {
+        // Arrange
+        var calculator = new Calc();
+
+        // Act
+        var result = calculator.Divide(num1, num2);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+}
