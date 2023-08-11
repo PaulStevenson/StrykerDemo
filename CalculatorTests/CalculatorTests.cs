e97b91a9d4e6848f82e3e1b1183aa36e18fb7a73
paulmcphailstevenson <paulmstevenson@pm.me>
2023-08-11 07:11:56 +0100

    Modified calc and tests so that stryker kills 100%

--- CalculatorTests/CalculatorTests.cs
+++ CalculatorTests/CalculatorTests.cs
@@ -4,8 +4,7 @@ namespace CalculatorTests;
 public class CalculatorTests
 {
     [Theory]
-    [InlineData(1, 0, 1)]
-    //[InlineData(1, 1, 2)]
+    [InlineData(5, 5, 10)]
     public void Add_ShouldAdd(int num1, int num2, int expected)
     {
         // Arrange
@@ -19,8 +18,8 @@ public class CalculatorTests
     }
 
     [Theory]
-    //[InlineData(1, 1, 0)]
-    [InlineData(1, 0, 1)]
+    [InlineData(5, 5, 0)]
+    // [InlineData(10, 5, 5)]
     public void Substract_ShouldSubtract(int num1, int num2, int expected)
     {
         // Arrange
@@ -35,6 +34,7 @@ public class CalculatorTests
 
     [Theory]
     [InlineData(1, 1, 1)]
+    // [InlineData(6, 3, 18)]
     public void Multiply_ShouldMultiply(int num1, int num2, int expected)
     {
         // Arrange
@@ -48,8 +48,10 @@ public class CalculatorTests
     }
 
     [Theory]
-    [InlineData(1, 1, 1)]
-    public void Divide_ShouldDivide(int num1, int num2, int expected)
+    [InlineData(1, 1, 1, 0)]
+    // [InlineData(20, 4, 5, 0)]
+    // [InlineData(9, 2, 4, 1)]
+    public void Divide_ShouldDivide(int num1, int num2, int expected, int expectedRemainder)
     {
         // Arrange
         var calculator = new Calc();
@@ -58,6 +60,15 @@ public class CalculatorTests
         var result = calculator.Divide(num1, num2);
 
         // Assert
-        Assert.Equal(expected, result);
+        Assert.Equal(expected, result.result);
+        Assert.Equal(expectedRemainder, result.remainder);
+    }
+
+    [Fact]
+    public void Divide_ShowThrow_WhenDivideByZero()
+    {
+        var calculator = new Calc();
+
+        Assert.Throws<DivideByZeroException>(() => calculator.Divide(1, 0));
     }
 }
