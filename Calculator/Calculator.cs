765b804edb81e7459e60ba74f49dc4627fc8672d
Robert Paul Stevenson <Paul688@bailliegifford.com>
2023-08-11 10:09:09 +0100

    Updated calculator and tests sp stryker score is 100%

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -1,4 +1,5 @@
 ﻿namespace Calculator;
+
 public class Calc
 {
     public int Add(int num1, int num2) => num1 + num2;
@@ -7,5 +8,11 @@ public class Calc
 
     public int Multiply(int num1, int num2) => num1 * num2;
 
-    public int Divide(int num1, int num2) => num1 / num2;
+    public (int result, int remainder) Divide(int num1, int num2)
+    {
+        var result = num1 / num2;
+        var remainder = num1 % num2;
+
+        return (result, remainder);
+    }
 }
--- CalculatorTests/CalculatorTests.cs
+++ CalculatorTests/CalculatorTests.cs
@@ -4,6 +4,7 @@ namespace CalculatorTests;
 public class CalculatorTests
 {
     [Theory]
+    [InlineData(1,0,1)]
     [InlineData(5, 5, 10)]
     public void Add_ShouldAdd(int num1, int num2, int expected)
     {
@@ -18,8 +19,9 @@ public class CalculatorTests
     }
 
     [Theory]
+    [InlineData(1, 0, 1)]
     [InlineData(5, 5, 0)]
-    // [InlineData(10, 5, 5)]
+    [InlineData(10, 5, 5)]
     public void Substract_ShouldSubtract(int num1, int num2, int expected)
     {
         // Arrange
@@ -34,7 +36,7 @@ public class CalculatorTests
 
     [Theory]
     [InlineData(1, 1, 1)]
-    // [InlineData(6, 3, 18)]
+    [InlineData(6, 3, 18)]
     public void Multiply_ShouldMultiply(int num1, int num2, int expected)
     {
         // Arrange
@@ -49,8 +51,8 @@ public class CalculatorTests
 
     [Theory]
     [InlineData(1, 1, 1, 0)]
-    // [InlineData(20, 4, 5, 0)]
-    // [InlineData(9, 2, 4, 1)]
+    [InlineData(20, 4, 5, 0)]
+    [InlineData(9, 2, 4, 1)]
     public void Divide_ShouldDivide(int num1, int num2, int expected, int expectedRemainder)
     {
         // Arrange
@@ -58,7 +60,6 @@ public class CalculatorTests
 
         // Act
         var result = calculator.Divide(num1, num2);
-
         // Assert
         Assert.Equal(expected, result.result);
         Assert.Equal(expectedRemainder, result.remainder);
